785ed426c29bef46d0bb260d3a0c4c0a6ab1383a
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2023-12-31 13:36:01 -0300

    Delete TrueDamageEnum.java

--- src/main/java/mymod/behaviours/spireModifications/TrueDamageEnum.java
+++
@@ -1,9 +0,0 @@
-package mymod.behaviours.spireModifications;
-
-import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-
-public class TrueDamageEnum {
-    @SpireEnum
-    public static DamageInfo.DamageType TRUE_DAMAGE;
-}
