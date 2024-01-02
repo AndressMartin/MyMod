29e3ce40eb832bd3ba23ea54919af448d13c91a7
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2024-01-02 01:19:18 -0300

    It`s actually working lol

--- src/main/java/mymod/behaviours/spireModifications/MapRoomNode_WingedPatch.java
+++ src/main/java/mymod/behaviours/spireModifications/MapRoomNode_WingedPatch.java
@@ -1,46 +1,36 @@
 package mymod.behaviours.spireModifications;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.ModHelper;
 import com.megacrit.cardcrawl.map.MapEdge;
 import com.megacrit.cardcrawl.map.MapRoomNode;
+import com.megacrit.cardcrawl.relics.WingBoots;
 import mymod.relics.InsidersMap;
 
 import java.util.Iterator;
 
 @SpirePatch2(
         clz= MapRoomNode.class,
-        method="update"
-
+        method="wingedIsConnectedTo"
 )
 public class MapRoomNode_WingedPatch {
 
-    public static boolean InsidersConnectedTo(MapRoomNode currentNode, MapRoomNode node){
-        Iterator var2 = currentNode.getEdges().iterator();
-        boolean hasRelic = AbstractDungeon.player.hasRelic(InsidersMap.ID);
+    @SpirePrefixPatch
+    public static SpireReturn<Boolean> WingedPatch(MapRoomNode __instance, MapRoomNode node){
+        Iterator var2 = __instance.getEdges().iterator();
+        boolean hasRelic = AbstractDungeon.player.hasRelic(InsidersMap.ID) ||
+                (AbstractDungeon.player.hasRelic(WingBoots.ID) ||
+                AbstractDungeon.player.getRelic(WingBoots.ID).counter > 0);
         MapEdge edge;
         do {
             if (!var2.hasNext()) {
-                return false;
+                return SpireReturn.Return(Boolean.FALSE);
             }
 
             edge = (MapEdge)var2.next();
-            if (ModHelper.isModEnabled("Flight") && node.y == edge.dstY) {
-                return true;
-            }
-        } while(node.y != edge.dstY || !hasRelic);
-
-        return true;
-    }
-
-    @SpireInsertPatch(
-        loc=276,
-        localvars={"wingedConnection"}
-    )
-    public static void InsertInsidersCheck(MapRoomNode __instance, boolean wingedConnection){
-        wingedConnection = InsidersConnectedTo(AbstractDungeon.getCurrMapNode(), __instance);
-        System.out.println(wingedConnection);
+        } while(node.y != edge.dstY -2 || !hasRelic || node.taken || !node.isConnectedTo(__instance));
+        return SpireReturn.Return(Boolean.TRUE);
     }
 }
